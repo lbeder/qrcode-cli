a7759ebd4c299ec3c5396e882b45339911e84d26
Leonid Beder <leonid.beder@gmail.com>
2019-02-22 16:06:44 +0200

    Convert data to a free parameter

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ use std::{env, path::Path, path::PathBuf, process::exit};
 pub struct CLIOptions {
     opts: QRCodeOptions,
     data: Vec<u8>,
-    outputPath: PathBuf,
+    output_path: PathBuf,
 }
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn print_usage(program: &str, opts: &Options) {
-    let brief = format!("Usage: {} v{} [options]", program, VERSION);
+    let brief = format!("Usage: {} v{} DATA [options]", program, VERSION);
     println!("{}", opts.usage(&brief));
 }
 
@@ -42,7 +42,6 @@ fn get_options() -> Options {
         ),
         "EC_LEVEL",
     );
-    opts.optopt("d", "data", "data to encode", "DATA");
     opts.optopt("o", "output", "output path for the image QR code", "OUTPUT");
     opts.optflag("t", "text", "embed the original data on the image QR code");
 
@@ -78,6 +77,14 @@ fn parse_options() -> CLIOptions {
         .and_then(|o| ECLevel::from_str(&o).ok())
         .unwrap_or(default_options.ec_level);
 
+    let data = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, &opts);
+        println!("Error: data is missing!");
+        exit(0);
+    };
+
     let path = match matches.opt_str("o") {
         Some(o) => o,
         None => {
@@ -87,21 +94,12 @@ fn parse_options() -> CLIOptions {
         }
     };
 
-    let data = match matches.opt_str("d") {
-        Some(o) => o,
-        None => {
-            print_usage(&program, &opts);
-            println!("Error: data is missing!");
-            exit(0);
-        }
-    };
-
     let embed = matches.opt_present("t");
 
     CLIOptions {
         opts: QRCodeOptions { ec_level, embed: embed },
         data: data.as_bytes().to_vec(),
-        outputPath: PathBuf::from(path),
+        output_path: PathBuf::from(path),
     }
 }
 
@@ -109,5 +107,5 @@ fn main() {
     let opts = parse_options();
 
     let qr = QRCode::new(&opts.opts);
-    qr.encode(&opts.data, &opts.outputPath).unwrap();
+    qr.encode(&opts.data, &opts.output_path).unwrap();
 }
